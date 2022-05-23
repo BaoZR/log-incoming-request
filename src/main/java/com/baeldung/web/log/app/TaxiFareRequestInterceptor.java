66b74e26d2a5ddc11470c9726cfe876ed919e5f2
bob Johnson <474234165@qq.com>
2022-05-23 23:07:10 +0800

    添加了计时功能

--- src/main/java/com/baeldung/web/log/app/TaxiFareRequestInterceptor.java
+++ src/main/java/com/baeldung/web/log/app/TaxiFareRequestInterceptor.java
@@ -11,6 +11,8 @@ import org.springframework.web.util.ContentCachingRequestWrapper;
 
 import com.baeldung.web.log.util.RequestLoggingUtil;
 
+import java.text.SimpleDateFormat;
+
 @Component
 public class TaxiFareRequestInterceptor extends HandlerInterceptorAdapter {
 
@@ -20,12 +22,18 @@ public class TaxiFareRequestInterceptor extends HandlerInterceptorAdapter {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String postData;
         HttpServletRequest requestCacheWrapperObject = null;
+        long startTime = System.currentTimeMillis();
+        request.setAttribute("startTime", startTime);
+
         try {
             // Uncomment to produce the stream closed issue
             //postData = RequestLoggingUtil.getStringFromInputStream(request.getInputStream());
 
             // To overcome request stream closed issue
             if(!(request instanceof ContentCachingRequestWrapper)) {
+                StringBuilder sb = new StringBuilder(1000);
+                sb.append("-----------------------开始计时:").append(new SimpleDateFormat("hh:mm:ss.SSS").format(startTime)).append("-------------------------------------\n");
+                LOGGER.debug(sb.toString());
                 requestCacheWrapperObject = new ContentCachingRequestWrapper(request);
             }
 
@@ -41,7 +49,16 @@ public class TaxiFareRequestInterceptor extends HandlerInterceptorAdapter {
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
         LOGGER.info("RESPONSE: " + response.getStatus());
+        long startTime = (Long) request.getAttribute("startTime");
+        long endTime = System.currentTimeMillis();
+        long executeTime = endTime - startTime;
+        StringBuilder sb = new StringBuilder(1000);
+        sb.append("CostTime  : ").append(executeTime).append("ms").append("\n");
+        sb.append("-------------------------------------------------------------------------------");
+        LOGGER.debug(sb.toString());
+
     }
 
 }
